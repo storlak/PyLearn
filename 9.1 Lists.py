19f55d79b683c1352f4e2fb6f1918b2c24c37d27
Serdar Torlak <serdartorlak@gmail.com>
2023-12-03 21:30:53 +0300

    Python learning notes, exercises.

---
+++ 9.1 Lists.py
@@ -0,0 +1,8 @@
+lale = [10, 20, 30, 40, 50]
+print(lale)
+print(type(lale))
+print(lale[0])
+print(len(lale))
+lale[-1] = "Hello"
+lale[2] = 35
+print(lale)
---
+++ 9.2 Tuples.py
@@ -0,0 +1,6 @@
+ev = ("masa", "sofra", "çatal")
+print(ev[1])
+
+ofis = ["masa", "sofra", "merdiven"]
+ofis = tuple(ofis) # liste ofisi tuple'a çevirdik.
+print(ofis)
