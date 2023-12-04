a67ceb4a8a7ac5cda0bca250e61026147c785210
Serdar Torlak <serdartorlak@gmail.com>
2023-12-04 23:40:17 +0300

    Python learning notes, exercises.

---
+++ 9.4 Slicing Sequences.py
@@ -0,0 +1,17 @@
+s = "Python rocks!"
+print(s[5])
+print(s[0:5])
+
+t = [1, 2, 3, 4, 5]
+print(t[1:4])
+t2 = t[:]  # this is shallow copy
+print(t2)
+print(t is t2)  # shallow copy is different. not same nor identical
+
+zoo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(zoo[1:8:2])
+print(zoo[0::2])
+print(zoo[1::2])
+print(zoo[-3:])
+zoo.reverse()
+print(zoo)
---
+++ 9.4.1 Sliciing Sequences.py
@@ -0,0 +1,8 @@
+s = "abcdef"
+print(s[-4:-1])
+print(s[-1:-4:-1])
+print(s[2::-1])
+print(s[::-1])
+
+a = "racecar"
+print(a == (a[::-1]))
