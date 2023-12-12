3381fbb5dea4d519d50b6a9aa262e56903271aa1
Kazure <serdartorlak@gmail.com>
2023-12-12 22:34:13 +0300

    Python learning notes, exercises.

---
+++ 10. Unicode Sets.py
@@ -0,0 +1,3 @@
+print("\N{Grinning Face}")
+print("\U0001F600")
+print("\U00000028") # don't forget to add 0 = U+0028
---
+++ 9.6 String Methods.py
@@ -0,0 +1,28 @@
+# Case Mapping
+print("Hello World".lower())
+print("Hello World".upper())
+print("one, two, three".title())
+
+s1 = "hello"
+s2 = "HeLlo"
+print(s1.casefold() == s2.casefold())
+
+# stripping
+# .lstrip() or .rstrip() or .strip() or .strip(" "), .strip("a","b","c")
+
+# concatenating
+print("Hello" + " " + "World")
+
+# spilliting
+data = "100, 200, 300, 400"
+print(data.split(","))  # returns a list of splitted elements.
+# joining
+print(",".join(["10", "20", "30"]))
+data = ("ab", "cd", "ef")
+print("--".join(data))
+# finding substrings
+# we use in
+# x in xyz -----> TRUE
+# a in xyz -----> FALSE
+
+# .startswith("...") or .endswith("...")
