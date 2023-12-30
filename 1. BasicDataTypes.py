16217bb0fe73937244f7fe9813183a0a61f90f81
Kazure <serdartorlak@gmail.com>
2023-12-30 13:14:55 +0300

    Small updates

--- 1. BasicDataTypes.py
+++ 1. BasicDataTypes.py
@@ -6,11 +6,13 @@ print(1.0)
 
 # float ile gözüken sayı aynısı değil, py yuvarlıyor. Örnek aşağıda. Gözüken 0.1 ama gerçek:
 print(format(0.1, ".25f"))
-print(0.1 == 0.1000000000000000055511151)  # aslında eşit değil ama True veriyor!
-print(0.1+0.1+0.1 == 0.3)  # false veriyor!!!
-print(abs(0.1+0.1+0.1 - 0.3)) # abs absolute value. Böylece aradaki farkı görebiliriz!
-print(format(0.1+0.1+0.1, ".25f"))
-print(0.1+0.1+0.1 == 0.3000000000000000444089210)  # == True!!!
+print(0.1 == 0.1000000000000000055511151)  # not equal or same but still True veriyor!
+print(0.1 + 0.1 + 0.1 == 0.3)  # false veriyor!!!
+print(
+    abs(0.1 + 0.1 + 0.1 - 0.3)
+)  # abs absolute value. Böylece aradaki farkı görebiliriz!
+print(format(0.1 + 0.1 + 0.1, ".25f"))
+print(0.1 + 0.1 + 0.1 == 0.3000000000000000444089210)  # == True!!!
 
 # Booleans
 print(True)
