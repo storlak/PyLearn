3d20813a422d855368f321b16c4c56f4fe774893
Kazure <serdartorlak@gmail.com>
2024-01-08 18:18:36 +0300

    Sort Method

---
+++ 21.1 Additional Functions - Sorted, Min, Max.py
@@ -0,0 +1,13 @@
+# sorted() Numbers
+# sorted by default ascending order
+# sorted alywas returns a new list so original iterable never mutated
+
+nums = [42, 67, 12, 43, 29, 9, 1]
+new_nums = sorted(nums)
+print(new_nums)
+
+t = (1, 5, 3, 7, 4, 9, 60)
+print(sorted(t))  # t is tuple but sorted returns a new list
+print(sorted(t, reverse=True))  # reverses in descending order
+
+# sorted() Strings
