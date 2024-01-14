764b6e741e17fad6ceeb972ed1ee90653e58bf2b
Kazure <serdartorlak@gmail.com>
2024-01-14 23:10:05 +0300

    000 updated.

--- 000.py
+++ 000.py
@@ -1,4 +1,8 @@
-students = ["Squidward", "SpongeBob", "Patrick", "Mr. Krabs"]
-students.sort()
-for i in students:
-    print(i)
+def is_even(x):
+    return x % 2 == 0
+
+
+numbers = [1, 2, 3, 4, 5, 6]
+even_numbers = filter(is_even, numbers)
+
+print(list(even_numbers))  # Output: [2, 4, 6]
