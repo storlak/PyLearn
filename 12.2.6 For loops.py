7c1a0da0f9329abb3d68beea35edc87d6cce4809
Kazure <serdartorlak@gmail.com>
2024-01-16 19:28:01 +0300

    updated

--- 12.2.6 For loops.py
+++ 12.2.6 For loops.py
@@ -1,2 +1,4 @@
-for x in reversed(range(1, 11)):  # using reversed
-    print(x)
+for x in range(3):
+    for y in reversed(range(1, 11)):  # using reversed
+        print(y, end=" ")  # end helps to have numbers in 1 line
+    print()
