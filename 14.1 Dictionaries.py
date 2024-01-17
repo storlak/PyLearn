05734e922e90ddc377aff5d9f88c82be97004d71
Kazure <serdartorlak@gmail.com>
2024-01-17 19:28:07 +0300

    Dictionary updated

--- 14.1 Dictionaries.py
+++ 14.1 Dictionaries.py
@@ -1,11 +1,9 @@
-person = {
-    "first_name": "Zoltan",
-    "last_name": "Kulle",
-    "year_born": 1989
-}
+person = {"first_name": "Zoltan", "last_name": "Kulle", "year_born": 1989}
 print(person["year_born"])
 person["year_born"] = 1888  # replaced in the dictionary
 print(person)
 person["month_born"] = "April"  # added to the dictionary
 print(person)
 
+# get gives the key. if Python can't find it it returns None.
+print(person.get("first_name"))
