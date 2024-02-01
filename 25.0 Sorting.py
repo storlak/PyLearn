08ebac3a1d9f651354ada9d6e8dcdf27cad5ebe9
Kazure <serdartorlak@gmail.com>
2024-02-02 00:22:09 +0300

    Sorting updated

--- 25.0 Sorting.py
+++ 25.0 Sorting.py
@@ -1,3 +1,5 @@
+# sorted(iterable, key=, reverse) sorted can take 3 args
+# sorted returns a new iterable
 data = [-6, 1 - 3, 8, -1, 7]
 print(sorted(data))
 print(sorted(data, key=lambda x: abs(x)))
