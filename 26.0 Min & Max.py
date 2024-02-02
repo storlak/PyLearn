f54ccb5c629304f6dfe53f8bd29d3d42b4a7e9d2
Kazure <serdartorlak@gmail.com>
2024-02-02 18:27:12 +0300

    Min & Max

---
+++ 26.0 Min & Max.py
@@ -0,0 +1,15 @@
+# min, max: (iterable, key=func)
+data = [-1, 6, 7, 0, 12, -9]
+
+print(min(data, key=abs))
+print(max(data))
+
+divisible_by_2 = [num for num in data if num % 2==0:]
+if divisible_by_2:
+    highest = max(divisible_by_2)
+    lowest = min(divisible_by_2)
+    print(f"Highest number divisible by 2 is {highest}")
+    print(f"Lowest number divisible by 2 is {lowest}")
+else:
+    print("There are no numbers divisible by 2 in the list")
+
