5bbaf331639d15db03d8ddd4da008cbea5b913fc
Kazure <serdartorlak@gmail.com>
2024-02-08 00:11:57 +0300

    CSV Files

--- 28.7 CSV Files.py
+++ 28.7 CSV Files.py
@@ -4,5 +4,5 @@ import csv
 with open("names.csv", "r") as my_file:
     csv_reader = csv.reader(my_file)
     for row in csv_reader:
-        print(row)  # this will print a [list] of data in csv
+        # print(row)  # this will print a [list] of data in csv
         print(row[2])  # we can print any index
