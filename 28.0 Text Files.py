348637a09bde633f20a2429f663c40897f68f2a8
Kazure <serdartorlak@gmail.com>
2024-02-07 12:57:31 +0300

    "r" mode updated

--- 28.0 Text Files.py
+++ 28.0 Text Files.py
@@ -1,4 +1,4 @@
-f = open("demofile.txt", "r")
+f = open("demofile.txt", "r")  # if no such file: returns an error.
 print(f.read())
 print(f.read(25))  # specify how many chars to return
 print(f.readline())  # returns a line
