e0cad2a040dd4e3e93eec87363d7185870205117
Kazure <serdartorlak@gmail.com>
2024-02-10 08:02:18 +0300

    Modules & İmport Updated

---
+++ 29.0 Modules & Imports.py
@@ -0,0 +1,11 @@
+# import - module name
+# from - module name import
+# import - module name as ...
+import math
+from math import sqrt  # can add ,
+import math as m
+
+x = math.sqrt(2)
+y = m.sqrt(6)
+print(x)
+print(y)
