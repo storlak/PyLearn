94e360996d15f3c756f8c0c72d8a2d178e0b0d3f
Kazure <serdartorlak@gmail.com>
2024-02-13 13:25:45 +0300

    DateTime Module Time, Date & DateTime

---
+++ 32.0 DateTime Module.py
@@ -0,0 +1,27 @@
+import datetime
+
+# date object
+
+dt = datetime.date(2024, 2, 13)
+dt1 = datetime.date.today()  # this is the local date. Not UTC date
+dt2 = datetime.date.fromisoformat("2020-12-31")
+print(dt)
+print(dt1)
+print(dt2)
+
+# time object
+
+t = datetime.time(13, 12, 28)
+t1 = datetime.time(0, 0, 0)  # midnight
+print(t)
+print(t.isoformat())
+print(t1)
+
+# datetime object
+st = datetime.datetime(2024, 2, 11, 13, 11, 38)
+st1 = datetime.datetime.now()  # local
+st2 = datetime.datetime.utcnow()  # utc
+
+print(st)
+print(st1)
+print(st2)
