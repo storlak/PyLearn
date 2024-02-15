218c52c6e257ef4bdf625cc0f72b39ca2e5de57a
Kazure <serdartorlak@gmail.com>
2024-02-15 14:51:29 +0300

    Converting timezon

--- 31.6 DateTime Module.py
+++ 31.6 DateTime Module.py
@@ -1,6 +1,16 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
-s = "2023-04-12T13:30:00-03:00"
+s = "2020-03-15T13:30:00-07:00"
 print(datetime.fromisoformat(s))
-td = timedelta(days=1, seconds=61200)
+td = timedelta(days=-1, seconds=61200)
 print(td.total_seconds())
+print(td.total_seconds() // 60 // 60)
+
+tz_EDT = timezone(timedelta(hours=-4), "EDT")
+tz_CDT = timezone(timedelta(hours=-5), "CDT")
+dt = datetime(year=2024, month=4, day=12, hour=22, minute=28, tzinfo=tz_EDT)
+print(dt)
+
+# converting EDT to CDT
+dt = dt.astimezone(tz_CDT)
+print(dt)
