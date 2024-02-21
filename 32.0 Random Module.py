16c815aafb72538c7e0046dc9bf39a7d9d841090
Kazure <serdartorlak@gmail.com>
2024-02-22 00:13:30 +0300

    Random module & Examples

--- 32.0 Random Module.py
+++ 32.0 Random Module.py
@@ -1,5 +1,18 @@
 import random
 
+# random.randint example:
+num = random.randint(1, 10)
+print(num)  # Output will be a random integer between 1 and 10, inclusive
+
+# random.random() example:
+num = random.random()
+print(num)  # Output will be a random float between 0.0 and 1.0
+
+#  random.randrange(start, stop, step)
+num = random.randrange(1, 10, 2)
+print(num)  # Output will be a random odd integer between 1 and 9
+
+# various examples:
 for _ in range(5):
     print(random.random())
 
