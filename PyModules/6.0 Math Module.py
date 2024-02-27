a181cec82e47725570b84ef504dbc1b66dbd4ad0
Kazure <serdartorlak@gmail.com>
2024-02-27 13:14:54 +0300

    Math Module

---
+++ PyModules/6.0 Math Module.py
@@ -0,0 +1,14 @@
+# math module most common functions
+import math
+
+power = math.pow(3, 4)
+square = math.sqrt(16)
+ceil = math.ceil(10.5)
+floor = math.floor(4.1)
+factorial = math.factorial(5)
+math.gcd(10, 20)  # greatest common diviser
+
+# fsum(iterable) it is like sum
+my_list = [1, 2, 3, 4]
+print(sum(my_list))
+# math module fsum works with floats better
