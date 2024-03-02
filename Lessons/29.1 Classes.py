fc94792f48dd548e8e023993b939455da2fead31
Kazure <serdartorlak@gmail.com>
2024-03-02 12:47:57 +0300

    Classes in Python

--- Lessons/29.1 Classes.py
+++ Lessons/29.1 Classes.py
@@ -1,15 +1,33 @@
-# to be concluded
-from math import pi
+# this part is from other sources
 
 
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
+# creating the class
+class SoftwareEngineer:
 
-    def area(self):
-        return pi * self.radius**2
+    # class attribute
+    alias = "Keyboard Magician"
+
+    # instance attributes
+    def __init__(self, name, age, level, salary):
+        self.name = name
+        self.age = age
+        self.level = level
+        self.salary = salary
 
 
-c = Circle(1)
-print(c.radius)
-print(c.area)
+# instance
+se1 = SoftwareEngineer("Max", 20, "Junior", 5000)
+se2 = SoftwareEngineer("Lisa", 25, "Junior", 800)
+print(se1.name, se1.age)
+print(SoftwareEngineer.alias)  # alias works with both. it is a class attribute
+print(se1.alias)
+print("*" * 15)
+print(se2.alias)
+print(SoftwareEngineer.alias)
+
+# Recap
+# creating a class
+# create an instance (object)
+# class vs instance
+# instance attributes: defined in __init__(self)
+# attribues
