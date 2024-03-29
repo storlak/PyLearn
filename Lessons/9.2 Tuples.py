bdcac1551a69267c2c46d3f19f86e9d64f5b9e37
Kazure <serdartorlak@gmail.com>
2024-03-30 02:42:51 +0300

    Converting list to tuple

--- Lessons/9.2 Tuples.py
+++ Lessons/9.2 Tuples.py
@@ -2,10 +2,14 @@ ev = ("masa", "sofra", "çatal")
 print(ev[1])
 
 ofis = ["masa", "sofra", "merdiven"]
-ofis = tuple(ofis)  # liste ofisi tuple'a çevirdik.
+ofis = tuple(ofis)  # converting list to tuple
 print(ofis)
 
-numbers = (10, 20, [30, 40])  # 2nd item is a list in a tuple. tuple is immutable but list not. we can not
+numbers = (
+    10,
+    20,
+    [30, 40],
+)  # 2nd item is a list in a tuple. tuple is immutable but list not. we can not
 # modify, change tuple item 2, but we can its objects! example:
 numbers[2][1] = 100
 print(numbers)
