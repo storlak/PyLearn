c58aef6da7c2094bc7b4e551f4444be74dd8945d
Kazure <serdartorlak@gmail.com>
2024-03-30 02:41:21 +0300

    Ternary operator

--- Lessons/8.1 TernaryOperator.py
+++ Lessons/8.1 TernaryOperator.py
@@ -1,10 +1,10 @@
-'''ask_price = 80
+"""ask_price = 80
 if ask_price > 50:
     volume = 50
 else:
-    volume = 80'''
+    volume = 80"""
 
+# below is the shortway of writing ternary operator
 ask_price = 100
 volume = 50 if ask_price > 50 else 80
 print(volume)
-
