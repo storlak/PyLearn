144b11f8154590cc772f4bbab9256fae0ea8ce5f
Kazure <serdartorlak@gmail.com>
2024-03-30 02:40:04 +0300

    Ternary operator

--- Lessons/8. TernaryOperator.py
+++ Lessons/8. TernaryOperator.py
@@ -1,8 +1,9 @@
-'''if price < 100
+"""if price < 100
     volume = 10
 else:
-    volume = 1'''
-# yukarıdakini aşağıdkai şekilde kısa olarak yazabiliriz.
+    volume = 1"""
+
+# below is the shortway of writing ternary operator
 
 product_price = 110
 volume = 100 if product_price < 100 else 1
