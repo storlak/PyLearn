2565a20d74d29b0925f6cc7cc046579124677d79
Kazure <serdartorlak@gmail.com>
2024-04-02 20:17:29 +0300

    Multi level inheritance

---
+++ Lessons/29.4 Classes.py
@@ -0,0 +1,19 @@
+# multi level inheritence by BroCode
+class Organism:
+    alive = True
+
+
+class Animal(Organism):
+    def eat(self):
+        print("This animal is eating.")
+
+
+class Dog(Animal):
+    def bark(self):
+        print("This dog is barking.")
+
+
+dog = Dog()
+print(dog.alive)  # inherited from Organism
+dog.eat()  # inherited from Animal
+dog.bark()  # inherited from Dog
