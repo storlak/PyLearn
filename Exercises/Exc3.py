b99a8a3ec31e0ad8c115bd8f5fb44c9329d8b0d5
Kazure <serdartorlak@gmail.com>
2024-04-03 16:00:47 +0300

    Moving the Exc folder to PyLearn-Exercises.

--- Exercises/Exc3.py
+++
@@ -1,9 +0,0 @@
-# Create two new variables that contain just the lower and upper case letters of `s` respectively, in the correct
-# alphabetical order, i.e:
-# "ABCDEF"
-# "abcdef"
-
-s = 'FfEeDdCcBbAa'
-reversed_s = s[::-1]
-print(reversed_s[::2])
-print(reversed_s[1::2])
