95a2b90ae316c1c923236e3874697fefc503ca3e
Kazure <serdartorlak@gmail.com>
2024-04-16 13:43:08 +0300

    is and == definition updated.

--- Lessons/5. ComparaisonOperators.py
+++ Lessons/5. ComparaisonOperators.py
@@ -5,8 +5,8 @@ c = 10.0
 print(id(a))
 print(a == b)
 print(a is b)
-print(a == c)  # value olarak True
-print(a is c)  # identity olarak False
+print(a == c)  # in terms of value it is True
+print(a is c)  # in terms ofidentity it is False
 
 
 class Vector:
@@ -16,10 +16,11 @@ class Vector:
 
     def __add__(self, other):
         if isinstance(other, Vector):
-            return Vector(self.x + other.x, + self.y + other.y)
+            return Vector(self.x + other.x, +self.y + other.y)
 
     def __repr__(self):
         return f"Vector ({self.x}, {self.y})"
+
 
 v1 = Vector(1, 1)
 v2 = Vector(1, 1)
