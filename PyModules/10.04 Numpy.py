ac1cad23f436d208cc5e00b10b63ea68dcb4cc15
Kazure <serdartorlak@gmail.com>
2024-05-09 21:22:54 +0300

    shape of array.

---
+++ PyModules/10.04 Numpy.py
@@ -0,0 +1,7 @@
+# shape of array
+
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+# this will print (2, 4)--> the array has 2 dimensions, where the first dimension has 2 elements and the second has 4.
+print(arr.shape)
