50c2ebead5c2fbbda9e531f81da01da461e74c52
Kazure <serdartorlak@gmail.com>
2024-05-10 00:59:40 +0300

    string data updated.

--- Lessons/1. BasicDataTypes.py
+++ Lessons/1. BasicDataTypes.py
@@ -6,7 +6,12 @@ print(1)
 # float
 print(1.0)
 
-# string & float rounding the numbers
+# string
+print("hello")
+print("I am from python".upper)
+my_name = input("Enter your name: ")
+print(f"Hello {my_name}".capitalize)
+
 # float number is not the same as integer number.Ex:  0.1 but it is not:
 print(format(0.1, ".25f"))
 print(0.1 == 0.1000000000000000055511151)  # not equal or same but still True!
