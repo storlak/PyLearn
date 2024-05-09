69cbd2a7d8c08d0658f25ce26d078e60b5855a5e
Kazure <serdartorlak@gmail.com>
2024-05-10 01:02:11 +0300

    Variables updated.

--- Lessons/3. Variables.py
+++ Lessons/3. Variables.py
@@ -6,3 +6,8 @@ b = a + 10
 print(b)
 
 # variable name and snake case is important
+my_name = "Zoltana"
+print(my_name)
+
+my_city, my_country = "Tokyo", "Japan"
+print(f"i live in {my_city}, and i am from {my_country}.")
