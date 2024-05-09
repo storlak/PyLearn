367469b8ef66a6b412d2501553f541acc8c8b9d2
Kazure <serdartorlak@gmail.com>
2024-05-09 22:08:27 +0300

    shape, size, dimension in arrays.

--- PyModules/10.04 Numpy.py
+++ PyModules/10.04 Numpy.py
@@ -5,3 +5,8 @@ import numpy as np
 arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 # this will print (2, 4)--> the array has 2 dimensions, where the first dimension has 2 elements and the second has 4.
 print(arr.shape)
+
+# reshaping arrays
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr.reshape(4, 3)
+print(newarr)
---
+++ PyModules/10.05 Numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+m2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+
+# elements in the array
+print(a.size)  # 3 elements in the array
+print(m2.size)  # 12 elements in the array
+# shape of the array
+print(a.shape)  # 1D array with 3 elements
+print(m2.shape)  # 2D array with 4 rows and 3 columns
