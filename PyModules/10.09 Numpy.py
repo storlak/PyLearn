ff47e236a5583a0506ae3e8cc49eafb1a9988eb3
Kazure <serdartorlak@gmail.com>
2024-05-14 11:04:56 +0300

    searching arrays

---
+++ PyModules/10.09 Numpy.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# searching arrays
+arr = np.array([1, 2, 3, 4, 5, 4, 4])
+x = np.where(arr == 4)
+print(x)  # the value 4 is present at index 3, 5, and 6.
+
+# find the indices where the values are even
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+x = np.where(arr % 2 == 0)
+print(x)
+
+# search sorted
+arr = np.array([6, 7, 8, 9])
+x = np.searchsorted(arr, 7)
+print(x)  # only 7 is present at index 1
