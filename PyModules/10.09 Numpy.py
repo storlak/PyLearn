cc82c44049fbddb83b697d3a958c115e1b155ef2
kazure <serdartorlak@gmail.com>
2024-05-15 17:16:32 +0300

    filtering&sorting arrays

--- PyModules/10.09 Numpy.py
+++ PyModules/10.09 Numpy.py
@@ -14,3 +14,12 @@ print(x)
 arr = np.array([6, 7, 8, 9])
 x = np.searchsorted(arr, 7)
 print(x)  # only 7 is present at index 1
+
+# search from right
+arr = np.array([6, 7, 8, 9])
+x = np.searchsorted(arr, 7, side="right")
+print(x)
+
+# sort arrays puts in ascending order
+arr = np.array([3, 2, 0, 1])
+print(np.sort(arr))
---
+++ PyModules/10.10 Numpy.py
@@ -0,0 +1,26 @@
+# filtering arrays
+import numpy as np
+
+arr = np.array([41, 42, 43, 44])
+x = [True, False, True, False]
+newarr = arr[x]
+print(newarr)
+
+# filter element higher than 42
+arr = np.array([41, 42, 43, 44])
+
+# Create an empty list
+filter_arr = []
+
+# go through each element in arr
+for element in arr:
+    # if the element is higher than 42, set the value to True, otherwise False:
+    if element > 42:
+        filter_arr.append(True)
+    else:
+        filter_arr.append(False)
+
+newarr = arr[filter_arr]
+
+print(filter_arr)
+print(newarr)
