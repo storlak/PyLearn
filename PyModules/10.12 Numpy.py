1af3ba89e795bee52d062d7e71e37917b53a102b
Kazure <serdartorlak@gmail.com>
2024-05-17 14:32:41 +0300

    vstack & hstack

--- PyModules/10.12 Numpy.py
+++ PyModules/10.12 Numpy.py
@@ -1,5 +1,19 @@
+# stacking arrays
+# vstack vertical and hstack horizontal
+
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5, 6])
-shap = arr.reshape(6, 1)
-print(shap)
+# colum numbers are equal so we can stack them(a1, a2)
+a1 = np.arange(1, 6)
+a2 = np.arange(1, 11).reshape(2, 5)
+
+# stacking arrays vertically
+s1 = np.vstack((a1, a2))
+print(s1)
+
+# stacking arrays horizontally using hstack
+u1 = np.linspace(0, 5, 10).reshape(2, 5)
+np.random.seed(0)
+u2 = np.random.randint(0, 10, 10).reshape(2, 5)
+result = np.hstack((u1, u2))
+print(result)
