9059baf2dd1dca132413e7ac537a54f1d77cb71e
Kazure <serdartorlak@gmail.com>
2024-06-27 12:12:44 +0300

    if cond updated

--- Lessons/7.1 if...else.py
+++ Lessons/7.1 if...else.py
@@ -1,16 +1,14 @@
-my_grade = 110
-
-if my_grade >= 90:
+my_grade = int(input("Enter your grade (0-100) : "))
+if my_grade > 100:
+    your_grade = "your grade cannot be greater than 100"
+elif my_grade >= 90:
     your_grade = "A"
+elif my_grade >= 80:
+    your_grade = "B"
+elif my_grade >= 70:
+    your_grade = "C"
+elif my_grade >= 60:
+    your_grade = "D"
 else:
-    if my_grade >= 80:
-        your_grade = "B"
-    else:
-        if my_grade >= 70:
-            your_grade = "C"
-        else:
-            if my_grade >= 60:
-                your_grade = "D"
-            else:
-                your_grade = "F"
+    your_grade = "F"
 print(your_grade)
